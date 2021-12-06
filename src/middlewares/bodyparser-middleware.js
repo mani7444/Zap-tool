b60153e4869e0853197ac6123415986a999a146d
mani7444 <87255850+mani7444@users.noreply.github.com>
2021-12-06 14:29:48 +0530

    Delete src directory

--- src/middlewares/bodyparser-middleware.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-const { urlencoded, json } = require("express");
-
-module.exports = (app) => {
-  app.use(json({ limit: "1mb" }));
-  return app.use(urlencoded({ extended: true }));
-};
